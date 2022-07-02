86bf88171eee40f1ced6ad6c1c1aadcefc981fe9
Emmanuel Onuorah <98954046+Emmanuel-webDev@users.noreply.github.com>
2022-07-02 23:12:18 +0000

    Changed some codes

--- src/assignment.js
+++ src/assignment.js
@@ -4,40 +4,24 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- **/
-function sumOfNumbers() {
+function sumOfNumbers(arrayOfNumbers) {
 const numbers = [1,2,3,4,5,6,7,8,9,10]
    const sum = numbers.reduce((acc, val)=>{
   return acc + val;
 });
 console.log(sum);
 }
+
 sumOfNumbers();
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
 
-function countEvenNumbers() {
-  const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-   const val  = arrOfNumbers.filter((number)=>{
+
+function countEvenNumbers(arrayOfNumbers) {
+   const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        const val  = arrOfNumbers.filter((number)=>{
             return number % 2 === 0
         })
-        console.log(val.length);
+        console.log(val.length)
 }
 countEvenNumbers();
 
